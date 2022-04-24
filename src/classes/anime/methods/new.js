a2d04d26a631698ccce31bf7d65c760ff0bb7685
Anibal Dams <elnini120@gmail.com>
2022-04-23 22:36:23 -0400

    .

--- src/classes/anime/methods/new.js
+++ src/classes/anime/methods/new.js
@@ -5,7 +5,7 @@ import {v4} from 'uuid';
 const _newAnime = async (
     name,
     synopsis,
-    color,
+    
     image,
     cover,
     releaseDate,
@@ -22,7 +22,7 @@ const _newAnime = async (
       id,
       name,
       synopsis,
-      color,
+      
       image,
       cover,
       releaseDate,
@@ -33,6 +33,7 @@ const _newAnime = async (
       type,
       private: Private,
     });
+    
     genres.forEach(async (el) => {
       const findGenre = await Genre.findOne({genre: el});
       if (findGenre) {
